2f83fd69b5c3f27c054d06e1d23889d4312e7657
Simon Huet <huetsimon35@gmail.com>
2019-07-28 00:37:23 +0200

    feat() : graphql query and react component 🚀

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
 import React from "react"
+import { StaticQuery } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const TitleAndDescription = ({data}) =>{
+    const title = data.site.siteMetadata.title
+    const description= data.site.siteMetadata.description
+
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
+const Header = () => <StaticQuery 
+                        query={graphql`
+                    query {
+                        site{
+                            seteMetadata{
+                                title
+                                description
+                            }
+                        }
+                    }
+                }`}
+                render={data => data.site.siteMetadata.title}/>
+
+const Layout = () => <div>
+    Hello world!
+    </div>
+
+export default Layout
